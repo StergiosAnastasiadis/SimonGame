55a1693f2f42ae22d8a553a5f415b4c0aac70075
Stergios Anastasiadis <44000859+StergiosAnastasiadis@users.noreply.github.com>
2020-04-20 00:06:50 +0200

    Second Step Completed

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
 //alert("Hallo"); //Test
+//Lonx Eisai
+var gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+function nextSequence (){
+  var randomNumber = Math.floor((Math.random() * 3 ));
+  var randomChoseColour = buttonColours[randomNumber];
+  gamePattern.push(randomChoseColour);
+
+  console.log(randomNumber);
+};
