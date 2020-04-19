148317a389d601d5a1c7e2eb138ac531da59249b
Stergios Anastasiadis <44000859+StergiosAnastasiadis@users.noreply.github.com>
2020-04-20 00:36:01 +0200

    Step 3 Completed

--- game.js
+++ game.js
@@ -1,11 +1,15 @@
-//alert("Hallo"); //Test
 //Lonx Eisai
 var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
+
 function nextSequence (){
-  var randomNumber = Math.floor((Math.random() * 3 ));
+  var randomNumber = Math.floor((Math.random() * 4 ));
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-  //$(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(150);
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+
   console.log(randomNumber);
 };
