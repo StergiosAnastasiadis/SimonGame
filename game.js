73052e14ea07167cec20f08194ef4b74fe832965
Stergios <Stelioslonx@outlook.com>
2020-04-20 21:32:37 +0200

    5.2 Completed

--- game.js
+++ game.js
@@ -7,15 +7,12 @@ function nextSequence() {
   var randomNumber = Math.floor((Math.random() * 4));
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-
   $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(150);
   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   audio.play();
-
   console.log(randomNumber);
   console.log(randomChosenColour);
 }
-
 
 $(".btn").click(function() {
   var userChosenColour = this.id;
@@ -28,5 +25,4 @@ $(".btn").click(function() {
 function playSound(name) {
   return name;
 }
-
 //https://github.com/StergiosAnastasiadis/SimonGame.git
