a138b988a6ba5500242ad508ae8d99c498c84680
Stergios Anastasiadis <44000859+StergiosAnastasiadis@users.noreply.github.com>
2020-04-22 01:15:15 +0200

    Step 6

--- game.js
+++ game.js
@@ -8,21 +8,21 @@ function nextSequence() {
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
   $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(150);
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-  audio.play();
+  window.audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  playSound(audio);
   console.log(randomNumber);
   console.log(randomChosenColour);
 }
 
 $(".btn").click(function() {
   var userChosenColour = this.id;
-  var audio = new Audio("sounds/" + userChosenColour + ".mp3");
-  audio.play();
+  audio = new Audio("sounds/" + userChosenColour + ".mp3");
+  playSound(audio);
   userClickedPattern.push(userChosenColour);
-  console.log(userChosenColour); //Just Testing
+  console.log(userChosenColour);
 });
 
 function playSound(name) {
-  return name;
+  audio.play(name);
 }
 //https://github.com/StergiosAnastasiadis/SimonGame.git
